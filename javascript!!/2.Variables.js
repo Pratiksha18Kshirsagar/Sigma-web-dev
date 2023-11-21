fdfafb0346bc066a891080eb6b55ebc66fcc7348
Pratiksha18Kshirsagar <149565116+Pratiksha18Kshirsagar@users.noreply.github.com>
2023-11-21 15:10:40 +0530

    21-11-2023

---
+++ javascript!!/2.Variables.js
@@ -0,0 +1,18 @@
+var a = 2;
+var b = "Pratiksha";
+var c = 3.03; //var is used globally!!
+console.log(a + b + c)
+console.log(typeof a , typeof b , typeof c) 
+
+
+const d = "kshirsagar";
+const e = 45;
+// e = e+1; not allowed ,const cannot be changed
+console.log(d);
+console.log(e);
+
+
+{
+let a = 12;  //let is used to a fixed block level.
+console.log(a);
+}
