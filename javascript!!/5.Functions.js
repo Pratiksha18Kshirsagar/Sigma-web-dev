e7b24e96437be1a8cf916882538bbcabe80b1f41
Pratiksha18Kshirsagar <149565116+Pratiksha18Kshirsagar@users.noreply.github.com>
2023-11-24 22:21:14 +0530

    24-11-2023

---
+++ javascript!!/5.Functions.js
@@ -0,0 +1,20 @@
+console.log("Hii lets learn  Functions!!")
+
+function pop(a) {
+    console.log("Heyy its me " , a  );
+    
+}
+pop("pratiksha");
+
+function sum(a ,b){
+    return a + b;
+}
+var box = sum(3,2);
+var box1 = sum(4,6);
+console.log("the sum is " , box);
+console.log("the sum is " , box1);
+
+const cop = () =>{
+    console.log("hii this is pratiksha!!" );
+}
+cop();
