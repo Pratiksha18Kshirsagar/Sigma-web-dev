96e6e6d26caa76ac85dc0ebaacf04ee499439f7e
Pratiksha18Kshirsagar <149565116+Pratiksha18Kshirsagar@users.noreply.github.com>
2023-11-26 20:09:38 +0530

    26-11-2023

---
+++ javascript!!/6.Strings.js
@@ -0,0 +1,19 @@
+var a = "Pratiksha"
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
+console.log(a[8]);
+
+console.log(a.replace("ksha", "ma"));
+console.log(a.concat("-pradeep", "-kshirsagar"));
+console.log(a.slice(0, 6));
+console.log(a.includes("P"));
+console.log(a.startsWith("Pra"));
+console.log(a.endsWith("ha"));
+console.log(a.length);
+console.log(a.charAt(0));
