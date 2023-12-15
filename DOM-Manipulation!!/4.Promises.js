bb5dce1259bb3ee43a9cc48ace46e6e2b211c912
Pratiksha18Kshirsagar <149565116+Pratiksha18Kshirsagar@users.noreply.github.com>
2023-12-15 23:00:48 +0530

    15-12-2023

---
+++ DOM-Manipulation!!/4.Promises.js
@@ -0,0 +1,19 @@
+console.log("I hate Promises!!");
+let a = new Promise((resolve, reject) => {
+    let x = 0;
+    if (x == 0) {
+        resolve("ok");
+    }
+    else {
+        reject("error");
+    }
+})
+
+a.then(
+    function (value) {
+        console.log((value));
+    },
+    function (error) {
+        console.log((error));
+    }
+);
