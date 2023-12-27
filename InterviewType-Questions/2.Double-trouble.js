0a39f2989425ee0517188073ff0410b558e4d5d6
Pratiksha18Kshirsagar <149565116+Pratiksha18Kshirsagar@users.noreply.github.com>
2023-12-27 08:18:31 +0530

    27-12-2023

---
+++ InterviewType-Questions/2.Double-trouble.js
@@ -0,0 +1,21 @@
+
+function doubleTheElement() {
+    let arr = [1, 1, 2, 3, 3, 4, 5, 1, 2];
+    let double = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == arr[i + 1]) {
+            const element = arr[i] * 2;
+            double.push(element);
+            i++;
+        }
+        else {
+            const element = arr[i ] * 2;
+            double.push(element);
+        }
+    }
+
+
+    console.log(double);
+}
+
+doubleTheElement();
