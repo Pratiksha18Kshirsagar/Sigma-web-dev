f183f3edc321dfb9574a959eaf1f3714a703afc6
Pratiksha18Kshirsagar <149565116+Pratiksha18Kshirsagar@users.noreply.github.com>
2024-01-04 23:57:25 +0530

    4-1-2024

---
+++ InterviewType-Questions/6.Vowels-count.js
@@ -0,0 +1,49 @@
+let str = 'PratikshaKshirsagarAIOU';
+let count = 0;
+
+// function countVowels(){
+//     for (const i of str) {
+//         if(i == 'a'){
+//             count = count+1
+//         }
+//         else if(i == 'A'){
+//             count++;
+//         }
+//         else if(i == 'E'){
+//             count++;
+//         }
+//         else if(i == 'e'){
+//             count++;
+//         }
+//         else if(i == 'I'){
+//             count++;
+//         }
+//         else if(i == 'i'){
+//             count++;
+//         }
+//         else if(i == 'O'){
+//             count++;
+//         }
+//         else if(i == 'o'){
+//             count++;
+//         }
+//         else if(i == 'u'){
+//             count++;
+//         }
+//         else if(i == 'U'){
+//             count++;
+//         }    
+//     }
+//     console.log(count);
+// }
+
+let vowels = 'aeiouAEIOU';
+function countVowels(){
+    for (const i of str) {
+        if(vowels.includes(i)){
+            count++
+        }
+    }
+    console.log(count);
+}
+countVowels();
