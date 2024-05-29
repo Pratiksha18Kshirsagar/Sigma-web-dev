84b0ddb27fa9ae4a26fe539882b5eee4a5abf8c9
Pratiksha18Kshirsagar <149565116+Pratiksha18Kshirsagar@users.noreply.github.com>
2024-05-29 09:59:12 +0530

    29-May

---
+++ Backend/main1.js
@@ -0,0 +1,8 @@
+//named export
+import {a,b,c,d,e} from "./myModule.js"
+console.log(e); 
+
+
+//default export
+import object from "./myModule.js"
+console.log(object.d);
---
+++ Backend/myModule.js
@@ -0,0 +1,14 @@
+export let  a = 3;
+export let  b = 32;
+export let  c = 43;
+export let  d = 36;
+export let  e = 73;
+
+let obj = {
+    a:12,
+    b:45,
+    c:33,
+    d:44
+}
+
+export default obj;
